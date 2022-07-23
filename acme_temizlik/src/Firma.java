8864d7e5b5d21ba26e5242486bc393ef548faeeb
aligirisen <agirisen0@gmail.com>
2022-07-23 17:13:21 +0300

    asd

---
+++ acme_temizlik/src/Firma.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author agiri
+ */
+public class Firma {
+    String firmaAdi;
+    int istedigiKidemli;
+    int istedigiKidemsiz;
+
+    public Firma(String firmaAdi, int istedigiKidemli, int istedigiKidemsiz) {
+        this.firmaAdi = firmaAdi;
+        this.istedigiKidemli = istedigiKidemli;
+        this.istedigiKidemsiz = istedigiKidemsiz;
+    }
+}
---
+++ acme_temizlik/src/Isci.java
@@ -0,0 +1,24 @@
+
+import java.util.ArrayList;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author agiri
+ */
+public class Isci {
+    ArrayList<String> calistigiFirmalar = new ArrayList<String>();
+    String isciAdi;
+    boolean kidem;
+
+    
+
+    public Isci(String isciAdi, boolean kidem) {
+        this.isciAdi = isciAdi;
+        this.kidem = kidem;
+    }
+}
